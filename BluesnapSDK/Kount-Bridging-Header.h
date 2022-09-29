bf8cccfaf330e6454c93f979666ab55b52d21f88
snpori <snpori@users.noreply.github.com>
2022-09-29 21:18:08 +0300

    Re-adding kount bridge headers

--- BluesnapSDK/Kount-Bridging-Header.h
+++ BluesnapSDK/Kount-Bridging-Header.h
@@ -1,7 +1,7 @@
 #ifndef Kount_Bridging_Header_h
 #define Kount_Bridging_Header_h
 
-//#import "KDataCollector.h"
-//#import "KountAnalyticsViewController.h"
+#import "KDataCollector.h"
+#import "KountAnalyticsViewController.h"
 
 #endif /* Kount_Bridging_Header_h */
